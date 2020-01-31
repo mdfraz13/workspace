8326930c5b5d6eb56428afb96919cbe5b8e23b4d
Mohammad Fraz <mohammad.fraz@intigral.net>
2020-01-31 11:02:28 +0400

    Array Based algos added

---
+++ AlgorithimsProjects/src/com/problemsolving/basics/arraybased/FindAFixedPoint.java
@@ -0,0 +1,20 @@
+package com.problemsolving.basics.arraybased;
+
+public class FindAFixedPoint {
+
+	public static void main(String[] args) {
+		int[] a = {-10, -5, 0, 3, 7};
+		int x = 2;
+		System.out.println(isFixedPointAvailable(a, x));
+
+	}
+
+	public static boolean isFixedPointAvailable(int[] a, int k){
+		int i =0;
+		while(i < a.length){
+			if(a[i] == k) return true;
+			i++;
+		}
+		return false;
+	}
+}
---
+++ AlgorithimsProjects/src/com/problemsolving/basics/arraybased/FindAPairWithGivenDifference.java
@@ -0,0 +1,40 @@
+package com.problemsolving.basics.arraybased;
+
+import java.util.Arrays;
+
+public class FindAPairWithGivenDifference {
+
+	public static void main(String[] args) {
+		int[] a = {5, 20, 3, 2, 50, 80};
+		int k = 78;
+
+		int[] res = findPairWithGivenDifference(a, k);
+		System.out.println(Arrays.toString(res));
+	}
+
+	public static int[] findPairWithGivenDifference(int[] a, int k){
+		int[] result = new int[2];
+
+		Arrays.sort(a); // n log n
+
+		int i = 0;
+		int j = 1;
+		while(i < a.length && j < a.length){
+			int temp = a[i] - a[j] ;
+			temp = temp > 0 ? temp : temp * -1;
+			System.out.println(temp);
+			if(temp < k){
+				j++;
+			}else if(temp > k){
+				i++;
+				j = 1;
+			}else{
+				result[0] = a[i];
+				result[1] = a[j];
+				break;
+			}
+		}
+
+		return result;
+	}
+}
